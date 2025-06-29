791865c90bf606313624efd0466b037ffeaeb46d
NatiPatati <kotynskanatalia.45@gmail.com>
2025-06-29 16:09:07 +0200

    Implement MovieService with basic CRUD operations

---
+++ src/main/java/nl/inholland/oop3/assignment700645/service/MovieService.java
@@ -0,0 +1,46 @@
+package nl.inholland.oop3.assignment700645.service;
+
+import nl.inholland.oop3.assignment700645.model.Movie;
+import nl.inholland.oop3.assignment700645.repository.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovieService implements IMovieService {
+
+    private final MovieRepository movieRepository;
+
+    @Autowired
+    public MovieService(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
+    @Override
+    public List<Movie> getAllMovies(int page, int size) {
+        return movieRepository.findAll(PageRequest.of(page, size)).getContent();
+    }
+
+    @Override
+    public Optional<Movie> getMovieById(Long id) {
+        return movieRepository.findById(id);
+    }
+
+    @Override
+    public Movie addMovie(Movie movie) {
+        return movieRepository.save(movie);
+    }
+
+    @Override
+    public Movie updateMovie(Movie movie) {
+        return movieRepository.save(movie);
+    }
+
+    @Override
+    public void deleteMovie(Long id) {
+        movieRepository.deleteById(id);
+    }
+}
