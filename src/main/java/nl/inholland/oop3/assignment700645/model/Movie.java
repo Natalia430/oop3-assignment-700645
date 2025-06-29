de18bee3ec530b203d3b268d3ba71e07678be77c
Natalia430 <kotynskanatalia.45@gmail.com>
2025-06-30 00:12:29 +0200

    Update Movie.java
    additions to movie class

--- src/main/java/nl/inholland/oop3/assignment700645/model/Movie.java
+++ src/main/java/nl/inholland/oop3/assignment700645/model/Movie.java
@@ -1,8 +1,107 @@
 package nl.inholland.oop3.assignment700645.model;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Movie {
-  public int getRating() {
-    return rating;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String year;
+    private String director;
+    private String genre;
+    private boolean watched;
+    private int rating; // 1 to 5
+    private String imagePath1;
+    private String imagePath2;
+    private String imagePath3;
+
+    public Movie() {
+    }
+
+    // Getters
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public boolean isWatched() {
+        return watched;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public String getImagePath1() {
+        return imagePath1;
+    }
+
+    public String getImagePath2() {
+        return imagePath2;
+    }
+
+    public String getImagePath3() {
+        return imagePath3;
+    }
+
+    // Setters
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public void setWatched(boolean watched) {
+        this.watched = watched;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public void setImagePath1(String imagePath1) {
+        this.imagePath1 = imagePath1;
+    }
+
+    public void setImagePath2(String imagePath2) {
+        this.imagePath2 = imagePath2;
+    }
+
+    public void setImagePath3(String imagePath3) {
+        this.imagePath3 = imagePath3;
+    }
 }
 
-}
